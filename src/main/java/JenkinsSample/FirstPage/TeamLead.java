56a8f3b7bd08570c231ccac90c8f7ba716e7b9c3
Kumar <sujay.kumar@accenture.com>
2018-10-31 13:11:32 +0530

    New files

---
+++ src/main/java/JenkinsSample/FirstPage/TeamLead.java
@@ -0,0 +1,12 @@
+package JenkinsSample.FirstPage;
+
+public class TeamLead extends Employee{
+	private int numberOfProjectsManaged;
+
+	public int getNumberOfProjectsManaged() {
+		return numberOfProjectsManaged;
+	}
+	public void setNumberOfProjectsManaged(int numberOfProjectsManaged) {
+		this.numberOfProjectsManaged = numberOfProjectsManaged;
+	}	
+}
